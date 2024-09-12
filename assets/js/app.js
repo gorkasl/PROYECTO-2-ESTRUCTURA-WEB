a40c38746045689beca6e0b73a056468c26a71a1
gorkasl <gsanchez@diariovasco.com>
2024-09-12 20:46:11 +0200

    clase_04

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import "./_scroll.js";
+
+
+const midocumentoHTML = document;
+midocumentoHTML.addEventListener('DOMContentLoaded',()=>{ 
+    /* todo lo que meta aquí dentro se ejecutará cuando se termine de cargar el DOM */
+});
